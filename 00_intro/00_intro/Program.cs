1a738c4c18d8bddd5cb4da29710812c2150e86ff
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-11-07 00:01:43 +0300

    intro

---
+++ 00_intro/00_intro/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _00_intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b = 34;
+
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ img/temp.cpp
@@ -0,0 +1,9 @@
+
+
+int a;
+
+int b = 45;
+
+int c(23);
+
+int d{ 12 };
