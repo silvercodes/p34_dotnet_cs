b0cc0ffbe89d57f915bfe05a93b88aa908ef5d2a
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-11-21 01:01:10 +0300

    add: arrays

--- img/temp.cpp
+++ img/temp.cpp
@@ -8,11 +8,27 @@
 
 // int d{ 12 };
 
-
+#include <iostream>
 int main()
 {
 
     int arr[10];
+
+    std::cout << arr;       // 0xAABB22
+    std::cout << arr + 1;   // 0xAABB26
+
+    std::cout << arr[1];    // *(arr + 1)
+
+    int* arrD = new int[10];
+    std::cout << arrD[1];   // *(arrD + 1)
+
+    int** arr2 = new int*[3];
+    for(int i{}; i < 3; ++i)
+        arr2[i] = new int[5];
+
+
+
+    
 
 
 }
