1454af75fc129a5ce49a8ee660472f40b907bfb6
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-01-30 00:14:22 +0300

    add: decorator

---
+++ 02_oop/03_dp_strategy/Program.cs
@@ -0,0 +1,75 @@
+﻿
+ICompression zipComp = new ZipCompression();
+ICompression rarComp = new RarCompression();
+ICompression defComp = new DeflateStrategy();
+
+Compressor compressor = new Compressor(zipComp);
+Console.WriteLine(compressor.makeArchive("data0.txt"));
+Console.WriteLine(compressor.makeArchive("data1.txt"));
+compressor.CompressionStrategy = rarComp;
+Console.WriteLine(compressor.makeArchive("data0.txt"));
+Console.WriteLine(compressor.makeArchive("data1.txt"));
+compressor.CompressionStrategy = defComp;
+Console.WriteLine(compressor.makeArchive("data0.txt"));
+Console.WriteLine(compressor.makeArchive("data1.txt"));
+
+abstract class ICompression
+{
+    public abstract string Compress(string path);
+    public abstract string Decompress(string path);
+}
+class ZipCompression : ICompression
+{
+    public override string Compress(string path)
+    {
+        return $"zip file: {path}.zip";
+    }
+
+    public override string Decompress(string path)
+    {
+        return $"recovered from zip file: {path}";
+    }
+}
+class RarCompression : ICompression
+{
+    public override string Compress(string path)
+    {
+        return $"rar file: {path}.rar";
+    }
+
+    public override string Decompress(string path)
+    {
+        return $"recovered from rar file: {path}";
+    }
+}
+class DeflateStrategy: ICompression
+{
+    public override string Compress(string path)
+    {
+        return $"deflate file: {path}.def";
+    }
+
+    public override string Decompress(string path)
+    {
+        return $"recovered from deflate file: {path}";
+    }
+}
+
+class Compressor
+{
+    public ICompression CompressionStrategy { get; set; }
+    public Compressor(ICompression compressionStrategy)
+    {
+        CompressionStrategy = compressionStrategy;
+    }
+
+    public string makeArchive(string path)
+    {
+        return CompressionStrategy.Compress(path);
+    }
+
+    public string recoveryFromArchive(string path)
+    {
+        return CompressionStrategy.Decompress(path);
+    }
+}
---
+++ 02_oop/04_decorator/Program.cs
@@ -0,0 +1,105 @@
+﻿
+//TextBox tb0 = new TextBox(150, 60);
+//tb0.Render();
+
+//IRenderable tb1 = new TextBox(150, 60);
+//tb1.Render();
+
+//IRenderable tb2 = new TextBox(150, 60);
+//IRenderable dec0 = new BorderDecorator(tb2);
+//dec0.Render();
+
+//IRenderable tb2 = new TextBox(150, 60);
+//IRenderable dec0 = new BorderDecorator(tb2, 3);
+//IRenderable dec1 = new ColoredDecorator(dec0);
+//dec1.Render();
+
+
+//IRenderable tb = new ScrollDecorator(new ColoredDecorator(new BorderDecorator(new TextBox(160, 70), 5)));
+//tb.Render();
+
+
+//abstract class IRenderable
+//{
+//    public abstract void Render();
+//}
+
+//class TextBox: IRenderable
+//{
+//    private int W { get; set; }
+//    private int H { get; set; }
+//    public TextBox(int w, int h)
+//    {
+//        W = w;
+//        H = h;
+//    }
+//    public override void Render()
+//    {
+//        Console.WriteLine($"TextBox: W: {W}, H: {H}");
+//    }
+//}
+
+//class Decorator: IRenderable
+//{
+//    private IRenderable Item { get; set; }
+//    public Decorator(IRenderable item)
+//    {
+//        Item = item;
+//    }
+//    public override void Render()
+//    {
+//        Item.Render();
+//    }
+//}
+//class BorderDecorator: Decorator
+//{
+//    private int thikness;
+//    public BorderDecorator(IRenderable item, int thikness):
+//        base(item)
+//    { 
+//        this.thikness = thikness;
+//    }
+//    public override void Render()
+//    {
+//        base.Render();
+//        Console.WriteLine($"Border {thikness} px");
+//    }
+//}
+//class ColoredDecorator : Decorator
+//{
+//    public ColoredDecorator(IRenderable item) :
+//        base(item)
+//    { }
+//    public override void Render()
+//    {
+//        base.Render();
+//        Console.WriteLine("Colored");
+//    }
+//}
+//class ScrollDecorator: Decorator
+//{
+//    public ScrollDecorator(IRenderable item) :
+//        base(item)
+//    { }
+//    public override void Render()
+//    {
+//        base.Render();
+//        Console.WriteLine("Scrolled");
+//    }
+//}
+
+
+#region Task
+// Написать мини-приложение для пиццерии на основе паттерна Decorator
+// 1. Пицца может 3-х разных размеров
+// 2. Пицца имеет название
+// 3. Возможные ингридиенты: помидоры, сыр, грибы, сырный бортик ...
+// Функционал: 
+//			создать любую пиццу из ингридиентов, 
+//			узнать стоимость пиццы
+//			напечатать чек (состав + стоимость)
+
+
+
+
+#endregion
